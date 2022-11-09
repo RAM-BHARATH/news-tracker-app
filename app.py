f295ebd32e8ded08a4c846e9ca3027326560cf55
RAM-BHARATH <ramtherapid007@gmail.com>
2022-11-09 15:21:47 +0530

    Home page, get_category, get_headlines

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from urllib.parse import quote_plus, urlencode
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for
+from flask import Flask, redirect, render_template, session, url_for, request
+
+from newsapi import NewsApiClient
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -14,6 +16,8 @@ app = Flask(__name__)
 # env_config = env.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 # app.config.from_object(env_config)
 app.secret_key = env.get("APP_SECRET_KEY")
+
+newsapi = NewsApiClient(api_key=env.get("NEWS_API_KEY"))
 
 oauth = OAuth(app)
 
@@ -27,6 +31,7 @@ oauth.register(
     server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
 )
 
+all_categories = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
 # @app.route("/")
 # def index():
 #     sample_config_data = app.config.get("SAMPLE")
@@ -62,13 +67,59 @@ def logout():
 
 @app.route("/")
 def home():
+    # all_articles = get_news()
+    all_articles = get_top_headlines_today()
     return render_template(
-        "home.html", 
+        # "home.html", 
+        "home_og.html",
         session=session.get('user'),
-        pretty=json.dumps(session.get('user'),
-        indent=4)
+        all_articles = all_articles,
+        categories= all_categories
+        # pretty=json.dumps(session.get('user'),indent=4)
     )
 
+@app.route("/news")
+def get_news():
+    all_articles = newsapi.get_everything(q='bitcoin',
+                                      sources='bbc-news,the-verge',
+                                      domains='bbc.co.uk,techcrunch.com',
+                                      from_param='2022-10-10',
+                                      to='2022-10-12',
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=1)
+    return all_articles
+
+@app.route("/top-headlines-today")
+def get_top_headlines_today():
+    user_categories = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
+    top_headlines = {}
+    for i in range(len(user_categories)):
+        top_headlines[user_categories[i]] = newsapi.get_top_headlines(
+                                                        # sources="",
+                                                        category=user_categories[i],
+                                                        language='en')
+        # top_headlines.append(newsapi.get_top_headlines(
+        #                                   category='business',
+        #                                   language='en'))
+        
+    return top_headlines
+
+@app.route("/user-info")
+def user_info():
+    return session.get('user')
+
+@app.route("/news-from-sources")
+def get_news_from_sources():
+    return 
+
+@app.route("/search", methods=['POST', 'GET'])
+def search_news():
+    search_query = request.form['search']
+    search_results = newsapi.get_everything(q=search_query)
+    # return search_query
+    return render_template('search_results.html', session=session.get('user'), search_results=search_results, 
+    categories=all_categories)
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=env.get("PORT", 5000))
-
